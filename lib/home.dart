e52017bfb7df77d5e0cd8cb7fab3323de74bee50
mxm381 <78032612+mxm381@users.noreply.github.com>
2023-03-22 15:19:36 +0100

    added list, tried image integration

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -11,7 +12,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> _devices = [];
+  List<String> _devices = ["schloss1", "schloss2"]; //connected smartlocks (hardcode at first)
   String _error_msg = "";
   final flutterReactiveBle = FlutterReactiveBle();
 
@@ -29,7 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  @override
+/*  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -54,5 +55,74 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.search),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}*/
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize:
+            Size.fromHeight(MediaQuery.of(context).size.height * 0.1),
+        child: AppBar(
+          //backgroundColor: FlutterFlowTheme.of(context).secondaryColor,
+          automaticallyImplyLeading: false,
+          title: Align(
+            alignment: AlignmentDirectional(0, 0),
+            child: Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(0, 25, 0, 0),
+              child: Text(
+                'SmartLock',
+                textAlign: TextAlign.center,
+                //style: FlutterFlowTheme.of(context).title1,
+              ),
+            ),
+          ),
+          actions: [],
+          centerTitle: false,
+          elevation: 2,
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: _devices.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(_devices[index]),
+            trailing: ElevatedButton(
+              onPressed: () {
+                // do something when button is pressed
+              },
+              child: Text('unlock'),
+            ),
+          );
+        },
+      ));
+      /*body: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              /*Image.asset(
+                'images/smartlock.png',
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.4,
+                fit: BoxFit.cover,
+              ),*/
+             ListView.builder(
+              itemCount: _devices.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_devices[index]),
+                  trailing: ElevatedButton(
+                    onPressed: () {
+                      // do something when button is pressed
+                    },
+                    child: Text('unlock'),
+                  ),
+                );
+              },
+            ), 
+            ]
+        ));*/
+      
   }
 }
