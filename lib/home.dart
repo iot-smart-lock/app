943b3e5a4afeeda4482ce7bb4d14fa7d63f9026f
Tom Freudenmann <tom.freudenmann@outlook.de>
2023-03-24 16:49:00 +0100

    ADD: Marker

--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
-import 'package:flutter_map/flutter_map.dart'; 
+import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-  
+
   final String title;
 
   @override
@@ -14,14 +13,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> _devices = ["schloss1", "schloss2", "schloss3"]; //connected smartlocks (hardcode at first)
+  List<String> _devices = [
+    "schloss1",
+    "schloss2",
+    "schloss3"
+  ]; //connected smartlocks (hardcode at first)
   String _error_msg = "";
-  final flutterReactiveBle = FlutterReactiveBle(); //BLE instance from imported package
+  final flutterReactiveBle =
+      FlutterReactiveBle(); //BLE instance from imported package
   final mapController = MapController(); //mapController from imported package
-  final apiKey = "233b12ac-968b-403a-b505-f1a2383ed99f"; //apiKey from stadiamaps; in example const
-  final styleUrl = "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"; //kein plan was des ist; in example auch const
+  final apiKey =
+      "233b12ac-968b-403a-b505-f1a2383ed99f"; //apiKey from stadiamaps; in example const
+  final styleUrl =
+      "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"; //kein plan was des ist; in example auch const
   int _selectedIndex = 0; //index for Navigation Bar
-  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
 
   //search function to find BLE devices
   void _search() {
@@ -41,35 +48,35 @@ class _MyHomePageState extends State<MyHomePage> {
   //frontend
   late final List<Widget> _widgetOptions = <Widget>[
     ListView.builder(
-              itemCount: _devices.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(_devices[index]),
-                  trailing: ElevatedButton(
-                    onPressed: () {
-                      // do something when button is pressed
-                    },
-                    child: Text('unlock'),
-                  ),
-                );
-              },
-            ), 
+      itemCount: _devices.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(_devices[index]),
+          trailing: ElevatedButton(
+            onPressed: () {
+              // do something when button is pressed
+            },
+            child: Text('unlock'),
+          ),
+        );
+      },
+    ),
     FlutterMap(
-      options: MapOptions(
-          center: LatLng(59.438484, 24.742595),
-          zoom: 14,
-          keepAlive: true
-      ),
-      children: [
+      options: MapOptions(center: LatLng(48, 9), zoom: 14, keepAlive: true),
+      children: <Widget>[
         TileLayer(
-          urlTemplate:
-          "$styleUrl?api_key={api_key}",
-          additionalOptions: {
-            "api_key": apiKey
-          },
+          urlTemplate: "$styleUrl?api_key={api_key}",
+          additionalOptions: {"api_key": apiKey},
           maxZoom: 20,
           maxNativeZoom: 20,
-        )
+        ),
+        MarkerLayer(
+          markers: [
+            Marker(
+                point: LatLng(48, 9),
+                builder: (context) => Icon(Icons.location_on)),
+          ],
+        ),
       ],
     )
   ];
